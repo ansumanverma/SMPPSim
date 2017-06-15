e871b89f78209a7aa61670bbe6cd87d1ca7c5a08
ansumanverma <anshuman.verma@gmail.com>
2017-06-15 04:24:39 +0100

    Fixing build failed

--- src/test/java/com/mycompany/smppsim/AppTest.java
+++ src/test/java/com/mycompany/smppsim/AppTest.java
@@ -1,9 +1,42 @@
 package com.mycompany.smppsim;
 
+
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.slf4j.LoggerFactory;
+
+import com.logica.smpp.Connection;
+import com.logica.smpp.Data;
+import com.logica.smpp.NotSynchronousException;
+import com.logica.smpp.Session;
+import com.logica.smpp.TCPIPConnection;
+import com.logica.smpp.TimeoutException;
+import com.logica.smpp.WrongSessionStateException;
+import com.logica.smpp.pdu.BindReceiver;
+import com.logica.smpp.pdu.BindRequest;
+import com.logica.smpp.pdu.BindTransciever;
+import com.logica.smpp.pdu.BindTransmitter;
+import com.logica.smpp.pdu.DeliverSM;
+import com.logica.smpp.pdu.EnquireLinkResp;
+import com.logica.smpp.pdu.PDU;
+import com.logica.smpp.pdu.PDUException;
+import com.logica.smpp.pdu.Request;
+import com.logica.smpp.pdu.Response;
+import com.logica.smpp.pdu.SubmitSM;
+import com.logica.smpp.pdu.SubmitSMResp;
+import com.logica.smpp.pdu.UnbindResp;
+import com.logica.smpp.util.ByteBuffer;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
+import tests.exceptions.BindReceiverException;
+import tests.exceptions.BindTransceiverException;
+import tests.exceptions.BindTransmitterException;
+import tests.exceptions.SubmitSmFailedException;
 /**
  * Unit test for simple App.
  */
